54039ee1bcd2b25dbe3a6dec7b5af087f85b79dc
ilasso <1204@holbertonschool.com>
2019-10-28 00:04:08 +0000

    %u

--- holberton.h
+++ holberton.h
@@ -1,6 +1,12 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdarg.h>
 
+int _process(char format, va_list arguments);
+void reverse(char *str, int len);
+int intToStr(int x, char str[]);
 int _printf(const char *format, ...);
+int binario(int num);
+unsigned int UintToStr(unsigned int x, char str[]);
 
 #endif /* HOLBERTON_H */
